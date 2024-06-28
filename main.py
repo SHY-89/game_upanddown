56347141001c0b2f5db3aa05fd040808ccec155f
SHY-89 <tjduwkrn@gmail.com>
2024-06-28 12:29:03 +0900

    게임 동작을 함수로 변경, 다시시작 및 강제종료 기능 구현 대기

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ def check_choice():
         choice = int(input("숫자를 입력 하세요:"))
     return choice
 
-
-user_select = check_choice()
-result = random.randint(1, 100)
-game_count = 1
-while user_select != result:
-
-    if user_select > result:
-        print("DOWN")
-    elif user_select < result:
-        print("UP")
+def check_game():
     user_select = check_choice()
-    game_count += 1
+    result = random.randint(1, 100)
+    game_count = 1
+    while user_select != result:
 
-print(f"시도 횟수 : {game_count} 입니다.")
+        if user_select > result:
+            print("DOWN")
+        elif user_select < result:
+            print("UP")
+        user_select = check_choice()
+        game_count += 1
+    return game_count
+
+
+count = check_game()
+print(f"시도 횟수 : {count} 입니다.")
+#check_roof = input("다시 게임을 시작 하시겠 습니까?(y/n)")
