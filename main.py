b77c4c12ae5d03e4e10ed283b8be1832739c0638
SHY-89 <tjduwkrn@gmail.com>
2024-06-28 12:26:23 +0900

    게임 룰 추가 및 룰 1번 적용

--- main.py
+++ main.py
@@ -3,18 +3,28 @@ import random
 print("Up Down GAME START")
 print("---- GAME RULE ----")
 print("1. 입력은 1~100까지의 숫자 중 하나만 입력이 가능합니다.")
+print("2. 게임을 포기 하시려면 END를 입력해 주시면 강제로 종료합니다.")
 print("-------------------")
 
-choice = int(input("숫자를 입력 하세요:"))
+
+def check_choice():
+    choice = int(input("숫자를 입력 하세요:"))
+    while choice < 1 or choice > 100:
+        print("GAME RULE 1번을 위배 하였습니다. 다시입력 하세요.")
+        choice = int(input("숫자를 입력 하세요:"))
+    return choice
+
+
+user_select = check_choice()
 result = random.randint(1, 100)
 game_count = 1
-while choice != result:
+while user_select != result:
 
-    if choice > result:
+    if user_select > result:
         print("DOWN")
-    elif choice < result:
+    elif user_select < result:
         print("UP")
-    choice = int(input("숫자를 입력 하세요:"))
+    user_select = check_choice()
     game_count += 1
 
 print(f"시도 횟수 : {game_count} 입니다.")
